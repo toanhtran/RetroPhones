c5dd312730a65f4cdb0a550d2ea961d100bf3714
toanhtran <toanh_tran@yahoo.com>
2015-12-20 21:25:46 +0900

    first commit

--- RetroPhones/ContactViewController.swift
+++ RetroPhones/ContactViewController.swift
@@ -10,26 +10,16 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(scrollView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSizeMake(375, 800)
     }
-    */
-
 }
--- RetroPhones/ProductViewController.swift
+++ RetroPhones/ProductViewController.swift
@@ -10,26 +10,16 @@ import UIKit
 
 class ProductViewController: UIViewController {
 
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "phone-fullscreen-3")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func addToCartPressed(sender: AnyObject) {
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
